aa4f0a7301fa7a8ae5b7cd9305b52770b2c6c445
unknown <43220811+vincentczhou@users.noreply.github.com>
2022-11-23 20:28:54 -0800

    eslint config

---
+++ src/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['react-app', 'react-app/jest', 'eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+    'no-unused-vars': 0,
+    'react/no-unescaped-entities': 0,
+  },
+}
